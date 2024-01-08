15a6c7c77dc359a424841b3ebad011301fcd4018
kogcyc <kogcyc@gmail.com>
2024-01-08 10:05:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import redis
 
 app = Flask(__name__)
 
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+r = redis.Redis(host='localhost', port=6379)
 
 @app.route('/')
 def index():
