a1e39ca082cccb328c5086227cc7bb95f4e485cb
kogcyc <kogcyc@gmail.com>
2024-01-08 12:13:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import os
 
 app = Flask(__name__)
 
+fp = open('data','r')
+datta = fp.read()
+fp.close()
+
 @app.route('/')
 def index():
-    return jsonify({"foo": 2})
+    return jsonify({"foo": datta})
 
 if __name__ == '__main__':
     app.run()
